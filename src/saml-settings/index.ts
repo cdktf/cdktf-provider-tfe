c8a89d6cef30f363de56af9595a273c7769f3d9f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-12-19 18:30:41 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@a335437

--- src/saml-settings/index.ts
+++ src/saml-settings/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/tfe/0.51.0/docs/resources/saml_settings
 // generated from terraform resource schema
 
