ee3f3cf360a3f6596bfaba1e88ca8d539f83db34
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-02-12 12:56:47 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@b24ef08

--- src/project-policy-set/index.ts
+++ src/project-policy-set/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/tfe/0.52.0/docs/resources/project_policy_set
 // generated from terraform resource schema
 
