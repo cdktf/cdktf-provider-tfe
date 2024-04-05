bd9d9dd1debc69a2754cdea5b68ff7c5aeded6e2
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-04-05 13:30:02 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/agent-pool/index.ts
+++ src/agent-pool/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/tfe/0.53.0/docs/resources/agent_pool
 // generated from terraform resource schema
 
