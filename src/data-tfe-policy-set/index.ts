66137946a1ea8ce83f14df44e8ff05ca23b0876b
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-30 03:54:41 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-tfe-policy-set/index.ts
+++ src/data-tfe-policy-set/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/tfe/0.57.1/docs/data-sources/policy_set
 // generated from terraform resource schema
 
--- src/workspace-run-task/index.ts
+++ src/workspace-run-task/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/tfe/0.57.1/docs/resources/workspace_run_task
 // generated from terraform resource schema
 
