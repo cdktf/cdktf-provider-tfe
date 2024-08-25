5f4d018794ac76864f03c57da5ebf58b574ffd11
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-25 22:14:42 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@d1a4f64

--- src/data-tfe-github-app-installation/index.ts
+++ src/data-tfe-github-app-installation/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/tfe/0.58.1/docs/data-sources/github_app_installation
 // generated from terraform resource schema
 
