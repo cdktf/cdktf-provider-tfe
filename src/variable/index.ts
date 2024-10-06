41aa6c87b4c6db2af9751624b6006f7f7c99f77e
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-06 06:12:53 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@2fcc6d9

--- src/variable/index.ts
+++ src/variable/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/tfe/0.59.0/docs/resources/variable
 // generated from terraform resource schema
 
