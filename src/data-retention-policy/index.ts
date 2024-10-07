823bdfeff128eda90873f7e37afd07d3d1ab8404
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-07 08:30:23 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-retention-policy/index.ts
+++ src/data-retention-policy/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/tfe/0.59.0/docs/resources/data_retention_policy
 // generated from terraform resource schema
 
